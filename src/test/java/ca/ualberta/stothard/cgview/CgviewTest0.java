fb7e1aa9386c3682047804896182b656c4ea3870
Paul Stothard <paul.stothard@gmail.com>
2020-06-14 22:23:10 -0600

    Initial commit

---
+++ src/test/java/ca/ualberta/stothard/cgview/CgviewTest0.java
@@ -0,0 +1,49 @@
+package ca.ualberta.stothard.cgview;
+
+import ca.ualberta.stothard.cgview.*;
+import java.io.*;
+
+public class CgviewTest0 implements CgviewConstants {
+   
+    public static void main( String args[] ) {
+
+	int length = 9000;
+	Cgview cgview = new Cgview(length);
+	
+	//some optional settings
+	cgview.setWidth(600);
+	cgview.setHeight(600);
+	cgview.setBackboneRadius(160.0f);
+	cgview.setTitle("Example");
+	cgview.setLabelPlacementQuality(10);
+	cgview.setShowWarning(true);
+	cgview.setLabelLineLength(8.0d);
+	cgview.setLabelLineThickness(0.5f);
+
+	//create a FeatureSlot to hold sequence features
+	FeatureSlot featureSlot = new FeatureSlot(cgview, DIRECT_STRAND);
+
+	//create random sequence features
+	for (int i = 1; i <= 100; i = i + 1) {
+
+	    int j = Math.round((float)((float)(length - 2) * Math.random())) + 1;
+	    
+	    //a Feature to add to our FeatureSlot
+	    Feature feature = new Feature(featureSlot, "label");
+
+	    //a single FeatureRange to add the Feature
+	    FeatureRange featureRange = new FeatureRange (feature, j, j + 1);
+	    featureRange.setDecoration(DECORATION_CLOCKWISE_ARROW);
+	    
+	}
+	
+	try {
+	    //create a PNG file
+	    CgviewIO.writeToPNGFile(cgview, "test_maps/CgviewTest0.png");
+	}
+        catch (IOException e) {
+	    e.printStackTrace(System.err);
+            System.exit(1);
+        }
+    }
+}
