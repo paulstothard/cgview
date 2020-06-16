86f8390e7150403b480d13a06f7f2705fbab16f7
Paul Stothard <paul.stothard@gmail.com>
2020-06-16 16:56:12 -0600

    Update license

--- src/main/java/ca/ualberta/stothard/cgview/FeatureRange.java
+++ src/main/java/ca/ualberta/stothard/cgview/FeatureRange.java
@@ -1,3 +1,20 @@
+/*   CGView - a Java package for generating high-quality, zoomable maps of
+ *   circular genomes.
+ *   Copyright (C) 2005 Paul Stothard stothard@ualberta.ca
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package ca.ualberta.stothard.cgview;
 
 import java.awt.*;
