ecfbabc32fbac22d2ca2c9a786a7469738d8aa3a
Paul Stothard <paul.stothard@gmail.com>
2021-05-21 18:50:00 -0600

    Make changes recommended by ShellCheck

--- build.sh
+++ build.sh
@@ -14,37 +14,32 @@ CGVIEW_JAR=$(find ./target -name "*jar-with-dependencies.jar" -print -quit)
 
 #test command-line processing of xml input
 echo "Testing command-line processing of xml files in 'sample_input'."
-XML_INPUT=($(find ./sample_input -name "*.xml" -type f))
-
-for file in "${XML_INPUT[@]}"; do
+find ./sample_input -name "*.xml" -type f | while IFS= read -r file; do
   f=$(basename "$file" .xml)
   echo "Processing file '$file'."
-
-  java -jar $CGVIEW_JAR -i "$file" -f png -o ./test_maps/"${f}".png
-  java -jar $CGVIEW_JAR -i "$file" -f svg -o ./test_maps/"${f}".svg
+  java -jar "$CGVIEW_JAR" -i "$file" -f png -o ./test_maps/"${f}".png
+  java -jar "$CGVIEW_JAR" -i "$file" -f svg -o ./test_maps/"${f}".svg
 done
+
 echo "Maps created in 'test_maps'."
 
 #test command-line processing of tab-delimited input
 echo "Testing command-line processing of tab files in 'sample_input'."
-TAB_INPUT=($(find ./sample_input -name "*.tab" -type f))
-
-for file in "${TAB_INPUT[@]}"; do
+find ./sample_input -name "*.tab" -type f | while IFS= read -r file; do
   f=$(basename "$file" .tab)
   echo "Processing file '$file'."
-
-  java -jar $CGVIEW_JAR -i "$file" -f png -o ./test_maps/"${f}".png
-  java -jar $CGVIEW_JAR -i "$file" -f svg -o ./test_maps/"${f}".svg
+  java -jar "$CGVIEW_JAR" -i "$file" -f png -o ./test_maps/"${f}".png
+  java -jar "$CGVIEW_JAR" -i "$file" -f svg -o ./test_maps/"${f}".svg
 done
 echo "Maps created in 'test_maps'."
 
 #test command-line creation of linked image series
 echo "Testing command-line creation of linked image series."
-java -jar $CGVIEW_JAR -i ./sample_input/xml/navigable.xml -s ./test_maps/navigable -x 1,6
+java -jar "$CGVIEW_JAR" -i ./sample_input/xml/navigable.xml -s ./test_maps/navigable -x 1,6
 echo "Maps created in 'test_maps/featureRange_element_series'."
 
 #create cgview.jar
-cp $CGVIEW_JAR ./target/cgview.jar
+cp "$CGVIEW_JAR" ./target/cgview.jar
 
 #add cgview.jar to ./bin
 cp ./target/cgview.jar ./bin/
